216f5f6439526305ddd95cf28ce551c2f0d3f452
BogdanAziev2003 <neazievbogdan@gmail.com>
2023-05-03 17:28:32 +0300

    modal v2.0

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let selectedFuel = "";
 let fuelList = document.querySelectorAll(".fuel");
 let modalWindow = document.querySelector(".modal__black");
 let modalExit = document.querySelector(".modal__exit");
+let modalRotate = document.querySelector(".modal__rotate");
+let modalParts = document.querySelectorAll(".modal__part p");
+
+
+let rotate = false;
 
 fuelList.forEach((fuel)=>{
     fuel.addEventListener("click", ()=>{
@@ -13,4 +18,10 @@ fuelList.forEach((fuel)=>{
 
 modalExit.addEventListener("click", ()=>{
     modalWindow.classList.add("_none");
+})
+
+modalRotate.addEventListener("click", ()=>{
+    let buffer = modalParts[0].innerText;
+    modalParts[0].innerText = modalParts[1].innerText
+    modalParts[1].innerText = buffer
 })
